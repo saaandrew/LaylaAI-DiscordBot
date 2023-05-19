f94f95cffcaeb5696a82dd183f5ffc4d6e4b29ff
QuynhNhuCoding.cpp <86618490+saaandrew@users.noreply.github.com>
2023-05-19 20:35:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ internet_access = True
 @bot.event
 async def on_ready():
     await bot.tree.sync()
-    await bot.change_presence(activity=discord.Game(name="Coded by Mishal#1916"))
+    await bot.change_presence(activity=discord.Game(name=" Garena Free Fire"))
     print(f"{bot.user} aka {bot.user.name} has connected to Discord!")
 
     invite_link = discord.utils.oauth_url(
@@ -334,7 +334,7 @@ async def help(ctx):
         command_description = command.description or "No description available"
         embed.add_field(name=command.name, value=command_description, inline=False)
     
-    embed.set_footer(text="Created by Mishal#1916")
+    embed.set_footer(text="aaaaaaaaaaaandrew")
 
     await ctx.send(embed=embed)
             
